2466c76d8083ce38f1d70e2ae442d94820956f4a
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-09-07 19:42:37 -0300

    Criado alguns metodos de comunicação com o servidor

--- server.ts
+++ server.ts
@@ -1,1 +1,17 @@
-console.log("alou!");
+import express from 'express'
+
+
+const app = express()
+
+const useres = []
+
+app.post('/usuarios', (req,res) =>{
+    console.log(req)
+    res.send('ok, POST aqui!!')
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send("Ok!")
+})
+
+app.listen(3000)
