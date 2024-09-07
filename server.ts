32594f67512de78ef1bbbacd7f63e83587e2740a
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-09-07 20:03:21 -0300

    ajustes para rotas post

--- server.ts
+++ server.ts
@@ -1,17 +1,20 @@
-import express from 'express'
+import express from 'express';
 
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-const useres = []
+const users = [];
 
-app.post('/usuarios', (req,res) =>{
-    console.log(req)
-    res.send('ok, POST aqui!!')
+app.post('/usuarios', (req: Request,res: Response) =>{
+    
+    users.push(req.body);
+
+    res.send('ok, POST aqui!!');
 })
 
-app.get('/usuarios', (req, res) => {
-    res.send("Ok!")
+app.get('/usuarios', (req: Request, res: Response) => {
+    res.json(users);
 })
 
 app.listen(3000)
