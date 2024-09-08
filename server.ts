1794e9a2befb43c383a7323795b040fb7d33de26
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-09-07 21:52:24 -0300

    Implementado consulta ao banco usando o prisma

--- server.ts
+++ server.ts
@@ -20,11 +20,14 @@ app.post('/usuarios', async (req,res) =>{
 
     })
     
-    res.status(201).json(req.body)
+    res.status(201).json(req.body);
 });
 
-app.get('/usuarios', (req, res) => {
-    res.json(users);
+app.get('/usuarios', async (req, res) => {
+    
+    const users = await prisma.user.findMany();
+
+    res.status(201).json(users);
 })
 
 app.listen(3000)
