f0c76baec273708f62eabce1b76194723f6dc13e
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-09-07 21:37:24 -0300

    implementado criacao de usuario dentro do banco utilizando o prisma

--- server.ts
+++ server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 
 const app = express();
@@ -6,11 +9,19 @@ app.use(express.json());
 
 const users = [];
 
-app.post('/usuarios', (req,res) =>{
+app.post('/usuarios', async (req,res) =>{
     
-    users.push(req.body);
-    res.status(201).json(req.body);
-})
+   await prisma.user.create({
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+
+    })
+    
+    res.status(201).json(req.body)
+});
 
 app.get('/usuarios', (req, res) => {
     res.json(users);
